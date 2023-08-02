a72a2d02d860ab168a467dad8b8261923d951295
Lyris Nunes <lyrisnunesdasilva@gmail.com>
2023-08-02 15:54:54 -0300

    Alteração fonte

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -38,7 +38,7 @@ export function Home(){
                return(
                   <article key={movies.id}>
                      <div className="box">
-                        <h3>{movies.title}</h3>
+                        <h2>{movies.title}</h2>
                         <img src={`https://image.tmdb.org/t/p/original/${movies.poster_path}`} alt={movies.title} />
                         <Link to={`/movie/${movies.id}`}>Ver mas</Link>
                      </div>
